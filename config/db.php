ce6c269272fe63a91bf87309f666e06d0b56b363
tianzewen <827697478@qq.com>
2016-07-20 09:51:42 +0800

    db gitgnore

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=meikeyun2016',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
